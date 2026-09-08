2ad7487fd6fbbf56de150c896da3b37d8c9a4efd
Lohith V K <lohithvk1842007@gmail.com>
2026-09-08 16:12:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n>999)
+        {
+            return n-1000+1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
