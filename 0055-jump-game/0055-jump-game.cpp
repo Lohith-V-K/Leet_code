5258273ac2714678ea265fa459d8c9a8ea198f59
Lohith V K <lohithvk1842007@gmail.com>
2026-09-12 19:38:11 +0530

    Time: 0 ms (100%), Space: 52.5 MB (16.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+         int maxindx=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i>maxindx)return false;
+            maxindx=max(maxindx,i+nums[i]);
+        }
+        return true;
+        
+    }
+};
